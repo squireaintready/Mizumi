3598a5c1abcc48877db5979856f2862c8e3c1e37
Samuel Jo <jo.sunghak@gmail.com>
2021-07-23 17:18:45 -0400

    implemented user inputs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
-import Header from './components/Header/Header'
+import "./App.css";
+import Header from "./components/Header/Header";
+import Inputs from "./components/Header/Inputs/Inputs";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
+      <div style={{marginTop:'1rem'}}>
+        <Inputs title={"Servers"} value={0} />
+        <Inputs title={"Bus Boys"} value={0} />
+        <Inputs title={"Bus Girls"} value={0} />
+        <Inputs title={"Total Tips"} value={0} />
+      </div>
     </div>
   );
 }
