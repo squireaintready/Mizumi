694f7e9bb0f89b9242404958918a00c107d1fa69
Samuel Jo <jo.sunghak@gmail.com>
2021-07-26 16:26:25 -0400

    added key prop to results

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,11 +1,10 @@
-import React from 'react'
 import Result from './Result'
 
 function Results({results}) {
   return (
     <div>
       {results.map(result =>(
-        <Result result={result}/>
+        <Result key={result.name} result={result}/>
       ))}
     </div>
   )
