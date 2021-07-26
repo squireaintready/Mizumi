0bc838726414098295d37959e778e8a3329a45d4
Samuel Jo <jo.sunghak@gmail.com>
2021-07-26 16:25:37 -0400

    refactored components into main content

---
+++ src/components/Content.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import Form from './Form'
+import Results from './Results'
+
+let results = [
+  {
+    name:'Servers',
+    value:0,
+  },
+  {
+    name:'Bus boys',
+    value:0,
+  },
+  {
+    name:'Bus girls',
+    value:0,
+  },
+  {
+    name:'Return',
+    value:0
+  }
+]
+
+
+function Content() {
+  return (
+    <div>
+      <Form/>
+      {results[0].value > 0
+      ? (
+        <Results results={results}/>
+      ): null}
+      
+    </div>
+  )
+}
+
+export default Content
