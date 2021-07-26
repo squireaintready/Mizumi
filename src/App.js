1faf7f5e88ba1d42787f5f65cb858835f4cc9219
Samuel Jo <jo.sunghak@gmail.com>
2021-07-26 16:23:25 -0400

    refactored inputs to content

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import "./App.css";
-import Header from "./components/Header/Header";
-import Inputs from "./components/Header/Inputs/Inputs";
+import Header from "./components/Header";
+import Content from "./components/Content";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <div style={{marginTop:'1rem'}}>
-        <Inputs title={"Servers"} value={0} />
-        <Inputs title={"Bus Boys"} value={0} />
-        <Inputs title={"Bus Girls"} value={0} />
-        <Inputs title={"Total Tips"} value={0} />
-      </div>
+      <Content/>
     </div>
   );
 }
